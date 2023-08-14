1cca15287136f9d5b915e7f8cfaca8c1d59d9abd
plabacki <plabacki97@gmail.com>
2023-08-14 18:32:24 +0200

    Add class to gets player choice of colors

--- mastermind.rb
+++ mastermind.rb
@@ -17,6 +17,25 @@ class ColorGuessGenerator
     end
 end
 
+class Player
 
-colorGenerator = ColorGuessGenerator.new()
-colorGenerator.generateGuess
+    attr_accessor :chosen_peg_of_colors
+
+    def initialize
+        @chosen_peg_of_colors = []
+    end
+
+    def generateGuess
+        number_of_color_to_guess = 4
+        number_of_color_to_guess.times do
+            color_guessed = gets.chomp
+            chosen_peg_of_colors.push(color_guessed) 
+        end
+        puts chosen_peg_of_colors    
+    end
+end        
+    
+#colorGenerator = ColorGuessGenerator.new()
+#colorGenerator.generateGuess
+player = Player.new()
+puts player.generateGuess
